06c5333bdc7ad6d5bbec168bd5b5d77ea2301a4b
SantishaR <82038785+SantishaR@users.noreply.github.com>
2021-08-20 19:48:41 +0200

    Added code for doubler.py

--- doubler.py
+++ doubler.py
@@ -4,3 +4,15 @@
 # where every element of the original array 
 # is multiplied by 2.
 
+def doubler():
+   numbers = [1, 2, 3, 4, 5]
+   doubled_numbers = []
+
+   for n in numbers:
+    new_number = n * 2
+    doubled_numbers.append(new_number)
+
+   print(doubled_numbers)
+
+
+doubler()
